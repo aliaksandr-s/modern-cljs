aeeb6eaa82709c02e5a6932a45b2d9f191051c2b
Alex S <s-a-n-9@mail.ru>
2019-06-25 22:32:01 +0300

    Add part-10

---
+++ part-10/modern-cljs/src/clj/modern_cljs/core.clj
@@ -0,0 +1,10 @@
+(ns modern-cljs.core
+  (:require [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found files resources]]
+            ))
+
+(defroutes handler
+  (GET "/" [] "Hello from Compojure")
+  (files "/" {:root "target"})
+  (resources "/" {:root "target"})
+  (not-found "Page Not Found"))
