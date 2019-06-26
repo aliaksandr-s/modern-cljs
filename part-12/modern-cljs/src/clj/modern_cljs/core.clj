a0391951fb115161af1ea60fcc552e043d11ca0e
Alex S <s-a-n-9@mail.ru>
2019-06-26 16:49:39 +0300

    Add part-12

---
+++ part-12/modern-cljs/src/clj/modern_cljs/core.clj
@@ -0,0 +1,11 @@
+(ns modern-cljs.core
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [not-found files resources]]
+            [modern-cljs.login :refer [authenticate-user]]))
+
+(defroutes handler
+  (GET "/" [] "Hello from Compojure")
+  (files "/" {:root "target"})
+  (POST "/login" [email password] (authenticate-user email password))
+  (resources "/" {:root "target"})
+  (not-found "Page Not Found"))
