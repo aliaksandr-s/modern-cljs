1f6fbe6c3b3363348625426b6bc8e524ace279dd
Alex S <s-a-n-9@mail.ru>
2019-06-28 16:34:39 +0300

    Add part-16

---
+++ part-16/modern-cljs/src/cljs/modern_cljs/shopping.cljs
@@ -0,0 +1,39 @@
+(ns modern-cljs.shopping
+  (:require [domina.core :refer [append!
+                                 by-class
+                                 by-id
+                                 destroy!
+                                 set-value!
+                                 value]]
+            [domina.events :refer [listen! prevent-default]]
+            [hiccups.runtime]
+            [shoreleave.remotes.http-rpc :refer [remote-callback]])
+  (:require-macros [hiccups.core :refer [html]]
+                   [shoreleave.remotes.macros :as macros]))
+
+(defn calculate [evt]
+  (let [quantity (value (by-id "quantity"))
+        price    (value (by-id "price"))
+        tax      (value (by-id "tax"))
+        discount (value (by-id "discount"))]
+    (remote-callback :calculate
+                     [quantity price tax discount]
+                     #(set-value! (by-id "total") (.toFixed % 2)))
+    (prevent-default evt)
+    ))
+
+(defn ^:export init []
+  (when (and js/document
+             (aget js/document "getElementById"))
+    (listen! (by-id "calc")
+             :click
+             (fn [evt] (calculate evt)))
+    (listen! (by-id "calc")
+             :mouseover
+             (fn []
+               (append! (by-id "shoppingForm")
+                        (html [:div.help "Click to calculate"]))))
+    (listen! (by-id "calc")
+             :mouseout
+             (fn []
+               (destroy! (by-class "help"))))))
